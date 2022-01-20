59cd05b91eba44684311209a9f9cec954070da89
Kamil W <kamkam@onet.eu>
2022-01-20 18:25:46 +0100

    Implement VNS with calls to main.solve(). Not fully working yet

--- LinearSolver/VNS.py
+++ LinearSolver/VNS.py
@@ -6,6 +6,18 @@ Authors: Maciej Morawski, Kamil Wiłnicki  2022
 
 import random
 import itertools
+import main
+
+#neighbourhood structures
+def generate_neighbourhood(x):
+    for k_set in range(1, K_MAX+1):
+        N_k = []
+        indices_list = list(range(SIZE))
+        indices_combination = itertools.combinations(indices_list, k_set)
+        for indices in indices_combination:
+            N_k.append(swap_indices(x[:], indices))
+        N.append(N_k)
+    #print (N[0])
 
 def swap_indices(v_old, indices_list):
     v_new = v_old
@@ -24,42 +36,38 @@ v_start = [
 
 SIZE = 20
 K_MAX = 7
-k = 1
-
-#neighbourhood structures
 N = []
 
-for k_set in range(1, K_MAX+1):
-    N_k = []
-    indices_list = list(range(SIZE))
-    indices_combination = itertools.combinations(indices_list, k_set)
-    for indices in indices_combination:
-        N_k.append(swap_indices(v_start[:], indices))
-    N.append(N_k)
-    
-#print (N[0])
+k = 1
+v_bis = []
 
-#while k == K_MAX:
-    
-'''
-import parameters
+generate_neighbourhood(v_start)
 
-params = parameters.define_parameters()
+while k < K_MAX:
+    #v_prim_index = N[k].index(random.choice(N[k]))
+    #v_prim = N[k][v_prim_index]
 
-def best_improvement(x):
-    while solve(x) > solve(x_a):
-        x_a = x
-        x = argmin(N(X)) #argmin in current neighbourhood
-    return x_a
+    v_prim = random.choice(N[k])
+    #print(v_prim)
+    v_prim_index = N[k].index(v_prim)
 
+    #solve greedily with result v_bis
+    if N[k][v_prim_index-1] is not None and main.solve(N[k][v_prim_index-1]) > main.solve(v_prim):
+        v_prim_index = v_prim_index - 1
+        v_prim = N[k][v_prim_index]
+    elif N[k][v_prim_index+1] is not None and main.solve(N[k][v_prim_index+1]) > main.solve(v_prim):
+        v_prim_index = v_prim_index + 1
+        v_prim = N[k][v_prim_index]
+    else:
+        v_bis = v_prim[:]
 
-kmax=5
-def VNS(x):
-    for k in range(kmax):
-        #shake
+    if main.solve(v_bis) > main.solve(v_start):
+        v_start = v_bis[:]
+        k = 1
+        print("generate neighbourhood")
+        generate_neighbourhood(v_start)
+    else:
+        k = k + 1
+        print("increment")
 
-        #bestimprovement
-
-        # neighbourhoodChange
-        print("vns")
-'''
+print(v_bis)
